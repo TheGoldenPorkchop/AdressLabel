f2c37d08c040e42bb82b83405f428d1b965ea68a
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-03-22 10:18:04 -0600

    Add project files.

---
+++ AdressLabel/AdressLabel.vb
@@ -0,0 +1,14 @@
+﻿'Angel Nava
+'Spring 2025
+'RCET2265
+'Adress Label
+'Link
+Option Strict On
+Option Explicit On
+
+
+Public Class AdressLabel
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
